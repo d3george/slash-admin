1457877905a5e754f10097a01a38404aa79f065d
d3george <dingsheng1214@gmail.com>
2023-10-05 21:17:32 +0800

    refactor: header transition height

--- src/layouts/dashboard/header.tsx
+++ src/layouts/dashboard/header.tsx
@@ -32,11 +32,12 @@ export default function Header({ className, offsetTop = false }: Props) {
   };
   return (
     <>
-      <header className={`z-20 w-full transition-height ${className}`} style={headerStyle}>
+      <header className={`z-20 w-full ${className}`} style={headerStyle}>
         <div
           className="shadow-2 flex flex-grow items-center justify-between px-4 text-gray backdrop-blur xl:px-6 2xl:px-10"
           style={{
             height: offsetTop ? '64px' : '80px',
+            transition: 'height 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
           }}
         >
           <div className="flex items-center ">
