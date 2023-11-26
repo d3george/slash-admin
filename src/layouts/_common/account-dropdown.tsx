7619370b71a17a54a74723ab6cc42556e240c6de
d3george <dingsheng1214@gmail.com>
2023-11-26 15:48:18 +0800

    refactor: account-dropdown items

--- src/layouts/_common/account-dropdown.tsx
+++ src/layouts/_common/account-dropdown.tsx
@@ -52,6 +52,14 @@ export default function AccountDropdown() {
 
   const items: MenuProps['items'] = [
     { label: <NavLink to="/dashboard">{t('sys.menu.dashboard')}</NavLink>, key: '0' },
+    {
+      label: <NavLink to="/management/user/profile">{t('sys.menu.user.profile')}</NavLink>,
+      key: '1',
+    },
+    {
+      label: <NavLink to="/management/user/account">{t('sys.menu.user.account')}</NavLink>,
+      key: '2',
+    },
     { type: 'divider' },
     {
       label: <button className="font-bold text-warning">{t('sys.login.logout')}</button>,
