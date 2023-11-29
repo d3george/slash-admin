54f8d96d20c46784efc7a9c33afcdd114fbd4545
d3george <dingsheng1214@gmail.com>
2023-11-29 23:03:10 +0800

    feat: error page demo

---
+++ src/router/routes/modules/errors.tsx
@@ -0,0 +1,60 @@
+import { Suspense, lazy } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { Iconify } from '@/components/icon';
+import { CircleLoading } from '@/components/loading';
+
+import { AppRouteObject } from '#/router';
+
+const Page403 = lazy(() => import('@/pages/sys/error/Page403'));
+const Page404 = lazy(() => import('@/pages/sys/error/Page404'));
+const Page500 = lazy(() => import('@/pages/sys/error/Page500'));
+
+const errors: AppRouteObject[] = [
+  {
+    path: 'error',
+    order: 6,
+    element: (
+      <Suspense fallback={<CircleLoading />}>
+        <Outlet />
+      </Suspense>
+    ),
+    meta: {
+      label: 'sys.menu.error.index',
+      icon: <Iconify icon="bxs:error-alt" className="ant-menu-item-icon" size="24" />,
+      key: '/error',
+    },
+    children: [
+      {
+        index: true,
+        element: <Navigate to="403" replace />,
+      },
+      {
+        path: '403',
+        element: <Page403 />,
+        meta: {
+          label: 'sys.menu.error.403',
+          key: '/error/403',
+        },
+      },
+      {
+        path: '404',
+        element: <Page404 />,
+        meta: {
+          label: 'sys.menu.error.404',
+          key: '/error/404',
+        },
+      },
+      {
+        path: '500',
+        element: <Page500 />,
+        meta: {
+          label: 'sys.menu.error.500',
+          key: '/error/500',
+        },
+      },
+    ],
+  },
+];
+
+export default errors;
