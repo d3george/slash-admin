6380b4eb7c8e584b54d6dd1dead4f00b2458cbfc
fliu2476 <link_a@foxmail.com>
2023-12-27 09:14:44 +0800

    fix: 多语言遗漏

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -40,7 +40,7 @@ axiosInstance.interceptors.response.use(
     }
 
     // 业务请求错误
-    throw new Error(message || 'sys.api.apiRequestFailed');
+    throw new Error(message || t('sys.api.apiRequestFailed'));
   },
   (error: AxiosError<Result>) => {
     const { response, message } = error || {};
