3e42bb8363949fbb3735abe657daa12446782d76
d3george <kevin@slashspaces.com>
2024-04-23 20:32:00 +0800

    feat: add docs link

--- src/layouts/_common/account-dropdown.tsx
+++ src/layouts/_common/account-dropdown.tsx
@@ -57,19 +57,27 @@ export default function AccountDropdown() {
   );
 
   const items: MenuProps['items'] = [
-    { label: <NavLink to={HOMEPAGE}>{t('sys.menu.dashboard')}</NavLink>, key: '0' },
+    {
+      label: (
+        <NavLink to="https://docs-admin.slashspaces.com/" target="_blank">
+          {t('sys.docs')}
+        </NavLink>
+      ),
+      key: '0',
+    },
+    { label: <NavLink to={HOMEPAGE}>{t('sys.menu.dashboard')}</NavLink>, key: '1' },
     {
       label: <NavLink to="/management/user/profile">{t('sys.menu.user.profile')}</NavLink>,
-      key: '1',
+      key: '2',
     },
     {
       label: <NavLink to="/management/user/account">{t('sys.menu.user.account')}</NavLink>,
-      key: '2',
+      key: '3',
     },
     { type: 'divider' },
     {
       label: <button className="font-bold text-warning">{t('sys.login.logout')}</button>,
-      key: '3',
+      key: '4',
       onClick: logout,
     },
   ];
