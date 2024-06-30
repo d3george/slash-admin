6c594992b8f342a17aa3acb3a3b2f083f6f95af8
d3george <kevin@slashspaces.com>
2024-06-30 11:37:49 +0800

    refactor: remove notification.success

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,7 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { App } from 'antd';
 import { useCallback } from 'react';
-import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { create } from 'zustand';
 
@@ -50,9 +49,8 @@ export const useUserPermission = () => useUserStore((state) => state.userInfo.pe
 export const useUserActions = () => useUserStore((state) => state.actions);
 
 export const useSignIn = () => {
-  const { t } = useTranslation();
   const navigatge = useNavigate();
-  const { notification, message } = App.useApp();
+  const { message } = App.useApp();
   const { setUserToken, setUserInfo } = useUserActions();
 
   const signInMutation = useMutation(userService.signin);
