c036eac10778906bed6142970f3bea13fd849160
xuwu <ixw1991@126.com>
2024-06-30 19:03:18 +0800

    style: enforce semicolon usage
    - Configure ESLint to require semicolons at the end of statements
    - Automatically add semicolons to vite-env.d.ts file

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GLOB_APP_TITLE: string
-  readonly VITE_APP_BASE_API: string
-  readonly VITE_APP_HOMEPAGE: string
-  readonly VITE_APP_ENV: 'development' | 'production'
+  readonly VITE_GLOB_APP_TITLE: string;
+  readonly VITE_APP_BASE_API: string;
+  readonly VITE_APP_HOMEPAGE: string;
+  readonly VITE_APP_ENV: 'development' | 'production';
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
