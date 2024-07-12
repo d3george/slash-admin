4a0e5e03d4756b36b3e4c2938b760034c11c618e
LingRay <12709688+LingRay@users.noreply.github.com>
2024-07-13 05:30:40 +0800

    fix: localized language initialization failed

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -2,14 +2,11 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-import { getStringItem } from '@/utils/storage';
-
 import en_US from './lang/en_US';
 import zh_CN from './lang/zh_CN';
 
 import { LocalEnum, StorageEnum } from '#/enum';
 
-const defaultLng = getStringItem(StorageEnum.I18N) || (LocalEnum.en_US as string);
 i18n
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
@@ -20,7 +17,10 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    lng: defaultLng, // localstorage -> i18nextLng: en_US
+    detection: {
+      // eg: zh-CN -> zh_CN
+      convertDetectedLanguage: (lng) => lng.replace('-', '_'),
+    },
     fallbackLng: LocalEnum.en_US,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
