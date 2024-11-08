afeeb581f71ad10bf122ad9c68f1c90f347be46b
d3george <dingsheng@dingshengdeMacBook-Pro.local>
2024-11-08 16:10:39 +0800

    refactor: replace collapse icon

--- src/layouts/dashboard/nav/nva-logo.tsx
+++ src/layouts/dashboard/nav/nva-logo.tsx
@@ -1,8 +1,9 @@
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 import Logo from '@/components/logo';
 import { useSettings } from '@/store/settingStore';
 import { useThemeToken } from '@/theme/hooks';
+import { cn } from '@/utils';
 
 import { HEADER_HEIGHT } from '../config';
 
@@ -14,7 +15,7 @@ type Props = {
 };
 export default function NavLogo({ collapsed, onToggle }: Props) {
   const { themeLayout } = useSettings();
-  const { colorPrimary, colorTextBase } = useThemeToken();
+  const { colorPrimary, colorBorder, colorTextSecondary, colorBgContainer } = useThemeToken();
   return (
     <div
       style={{ height: `${HEADER_HEIGHT}px` }}
@@ -28,13 +29,23 @@ export default function NavLogo({ collapsed, onToggle }: Props) {
           </span>
         )}
       </div>
-      <button
+      <div
         onClick={onToggle}
-        className="absolute right-0 top-7 z-50 hidden h-6 w-6 translate-x-1/2 cursor-pointer select-none rounded-full text-center !text-gray md:block"
-        style={{ color: colorTextBase, borderColor: colorTextBase, fontSize: 16 }}
+        className={cn(
+          'absolute right-0 top-7 z-50 hidden h-6 w-6 translate-x-1/2 cursor-pointer select-none items-center justify-center rounded-full text-center md:flex',
+        )}
+        style={{
+          fontSize: 16,
+          border: `1px solid ${colorBorder}`,
+          backgroundColor: colorBgContainer,
+        }}
       >
-        {collapsed ? <MenuUnfoldOutlined size={20} /> : <MenuFoldOutlined size={20} />}
-      </button>
+        {collapsed ? (
+          <RightOutlined style={{ fontSize: 12, color: colorTextSecondary }} />
+        ) : (
+          <LeftOutlined style={{ fontSize: 12, color: colorTextSecondary }} />
+        )}
+      </div>
     </div>
   );
 }
