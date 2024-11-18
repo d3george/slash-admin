2439aecd56b558f1d0738dd72f05241c5531eeb5
weiShaoY_ <1604705673@qq.com>
2024-11-18 15:57:30 +0800

    fix: Fix iframe white screen

--- src/pages/sys/others/iframe/index.tsx
+++ src/pages/sys/others/iframe/index.tsx
@@ -1,10 +1,35 @@
+import { Spin } from "antd";
+import { useState } from "react";
+
 type Props = {
 	src: string;
 };
+
 export default function Iframe({ src = "" }: Props) {
+	// 使用 state 来控制 iframe 是否加载完成
+	const [isLoading, setIsLoading] = useState(true);
+
+	// 当 iframe 加载完成时，更新 isLoading 状态
+	const handleLoad = () => {
+		setIsLoading(false);
+	};
+
 	return (
-		<div className="h-full w-full">
-			<iframe src={src} title="iframe-page" className="h-full w-full" />
+		<div className="h-full w-full relative">
+			{/* 加载中时显示的 loading 效果 */}
+			{isLoading && (
+				<div className="absolute inset-0 flex items-center justify-center bg-white z-10">
+					{/* <p>Loading...</p> */}
+					<Spin size="large" />
+				</div>
+			)}
+			{/* iframe 元素 */}
+			<iframe
+				src={src}
+				title="iframe-page"
+				className="h-full w-full"
+				onLoad={handleLoad} // 监听 iframe 加载完成事件
+			/>
 		</div>
 	);
 }
