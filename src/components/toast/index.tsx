ca69f3a552478b88db753d022fb2ce6b111d31d6
d3george <kevin@slashspaces.com>
2024-11-20 10:46:46 +0800

    feat: use sonner replace antd message + alert + notification

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -1,7 +1,9 @@
 import { useSettings } from "@/store/settingStore";
 import { useThemeToken } from "@/theme/hooks";
 
+import type { GlobalToken } from "antd";
 import { Toaster } from "sonner";
+import styled from "styled-components";
 import Iconify from "../icon/iconify-icon";
 
 /**
@@ -9,72 +11,155 @@ import Iconify from "../icon/iconify-icon";
  */
 export default function Toast() {
 	const { themeMode } = useSettings();
+	const theme = useThemeToken();
 	const {
 		colorInfo,
 		colorSuccess,
 		colorWarning,
 		colorError,
 		colorBgContainer,
-	} = useThemeToken();
+	} = theme;
 
 	return (
-		<Toaster
-			position="top-right"
-			theme={themeMode}
-			toastOptions={{
-				duration: 3000,
-				style: {
-					backgroundColor: colorBgContainer,
-				},
-				classNames: {
-					toast: "flex  rounded-lg border-0",
-					description: "text-xs text-current/45",
-					icon: "flex items-center justify-center px-4 rounded-lg",
-					success: "bg-success/10",
-					error: "bg-error/10",
-					warning: "bg-warning/10",
-					info: "bg-info/10",
-				},
-			}}
-			icons={{
-				success: (
-					<div className="p-2 bg-success/10 rounded-lg">
-						<Iconify
-							icon="carbon:checkmark-filled"
-							size={24}
-							color={colorSuccess}
-						/>
-					</div>
-				),
-				error: (
-					<div className="p-2 bg-error/10 rounded-lg">
-						<Iconify
-							icon="carbon:warning-hex-filled"
-							size={24}
-							color={colorError}
-						/>
-					</div>
-				),
-				warning: (
-					<div className="p-2 bg-warning/10 rounded-lg">
-						<Iconify
-							icon="carbon:warning-alt-filled"
-							size={24}
-							color={colorWarning}
-						/>
-					</div>
-				),
-				info: (
-					<div className="p-2 bg-info/10 rounded-lg">
-						<Iconify
-							icon="carbon:information-filled"
-							size={24}
-							color={colorInfo}
-						/>
-					</div>
-				),
-			}}
-			expand
-		/>
+		<ToasterStyleWrapper $theme={theme}>
+			<Toaster
+				position="top-right"
+				theme={themeMode}
+				toastOptions={{
+					duration: 3000,
+					style: {
+						backgroundColor: colorBgContainer,
+					},
+					classNames: {
+						toast: "rounded-lg border-0",
+						description: "text-xs text-current/45",
+						content: "flex-1 ml-2",
+						icon: "flex items-center justify-center px-4 rounded-lg",
+						success: "bg-success/10",
+						error: "bg-error/10",
+						warning: "bg-warning/10",
+						info: "bg-info/10",
+					},
+				}}
+				icons={{
+					success: (
+						<div className="p-2 bg-success/10 rounded-lg">
+							<Iconify
+								icon="carbon:checkmark-filled"
+								size={24}
+								color={colorSuccess}
+							/>
+						</div>
+					),
+					error: (
+						<div className="p-2 bg-error/10 rounded-lg">
+							<Iconify
+								icon="carbon:warning-hex-filled"
+								size={24}
+								color={colorError}
+							/>
+						</div>
+					),
+					warning: (
+						<div className="p-2 bg-warning/10 rounded-lg">
+							<Iconify
+								icon="carbon:warning-alt-filled"
+								size={24}
+								color={colorWarning}
+							/>
+						</div>
+					),
+					info: (
+						<div className="p-2 bg-info/10 rounded-lg">
+							<Iconify
+								icon="carbon:information-filled"
+								size={24}
+								color={colorInfo}
+							/>
+						</div>
+					),
+					loading: (
+						<div className="p-2 bg-gray-400/10 text-gray-400 rounded-lg">
+							<Iconify
+								icon="svg-spinners:6-dots-scale-middle"
+								size={24}
+								speed={3}
+							/>
+						</div>
+					),
+				}}
+				expand
+			/>
+		</ToasterStyleWrapper>
 	);
 }
+
+const ToasterStyleWrapper = styled.div<{ $theme: GlobalToken }>`
+  [data-sonner-toast] {
+    font-weight: 600;
+    font-size: 14px;
+
+    [data-cancel] {
+      color: ${(props) => props.$theme.colorTextBase};
+      background-color: transparent;
+      &:hover {
+        background-color: ${(props) => props.$theme.colorBgTextHover};
+      }
+    }
+
+    /* Default */
+    [data-action] {
+      color: ${(props) => props.$theme.colorPrimary};
+      background-color: transparent;
+      &:hover {
+        background-color: ${(props) => props.$theme.colorPrimaryBgHover};
+      }
+    }
+
+    /* Info */
+    &[data-type="info"] [data-action] {
+      color: ${(props) => props.$theme.colorInfo};
+      background-color: transparent;
+      &:hover {
+        background-color: ${(props) => props.$theme.colorInfoBgHover};
+      }
+    }
+
+    /* Error */
+    &[data-type="error"] [data-action] {
+      color: ${(props) => props.$theme.colorError};
+      background-color: transparent;
+      &:hover {
+        background-color: ${(props) => props.$theme.colorErrorBgHover};
+      }
+    }
+
+    /* Success */
+    &[data-type="success"] [data-action] {
+      color: ${(props) => props.$theme.colorSuccess};
+      background-color: transparent;
+      &:hover {
+        background-color: ${(props) => props.$theme.colorSuccessBgHover};
+      }
+    }
+
+    /* Warning */
+    &[data-type="warning"] [data-action] {
+      color: ${(props) => props.$theme.colorWarning};
+      background-color: transparent;
+      &:hover {
+        background-color: ${(props) => props.$theme.colorWarningBgHover};
+      }
+    }
+
+    /* Close Button */
+    [data-close-button] {
+      top: 0;
+      right: 0;
+      left: auto;
+      border: 1px solid ${(props) => props.$theme.colorBorder};
+      border-style: dashed;
+      background-color: ${(props) => props.$theme.colorBgContainer};
+    }
+  }
+`;
--- src/pages/components/toast/index.tsx
+++ src/pages/components/toast/index.tsx
@@ -2,10 +2,28 @@ import { Button, Card, Flex } from "antd";
 import { toast } from "sonner";
 
 export default function ToastPage() {
+	const promise = () =>
+		new Promise((resolve) =>
+			setTimeout(() => resolve({ name: "Sonner" }), 2000),
+		);
+
+	const handleSubmit = () => {
+		toast.promise(promise, {
+			loading: "Loading...",
+			success: (data: any) => {
+				return `${data.name} toast has been added`;
+			},
+			error: "Error",
+		});
+	};
 	return (
 		<Flex gap={16} vertical wrap>
 			<Flex gap={16} wrap>
-				<Card title="Simple" bordered={false} className="flex-none lg:flex-1">
+				<Card
+					title="Simple"
+					bordered={false}
+					className="flex-none text- lg:flex-1 bg-"
+				>
 					<Flex gap={16} wrap>
 						<Button
 							color="default"
@@ -53,17 +71,163 @@ export default function ToastPage() {
 						</Button>
 					</Flex>
 				</Card>
-				<Card title="Info" bordered={false} className="flex-none lg:flex-1">
-					Info
+				<Card
+					title="With Action"
+					bordered={false}
+					className="flex-none lg:flex-1"
+				>
+					<Flex gap={16} wrap>
+						<Button
+							color="default"
+							variant="solid"
+							onClick={() =>
+								toast("Toast Default", {
+									cancel: {
+										label: "Cancel",
+										onClick: () => {},
+									},
+									action: {
+										label: "Action",
+										onClick: () => {},
+									},
+								})
+							}
+						>
+							Default
+						</Button>
+						<Button
+							color="default"
+							className="!bg-info"
+							variant="solid"
+							onClick={() =>
+								toast.info("Toast Info", {
+									action: {
+										label: "Action",
+										onClick: () => {},
+									},
+									cancel: {
+										label: "Cancel",
+										onClick: () => {},
+									},
+								})
+							}
+						>
+							Info
+						</Button>
+						<Button
+							color="default"
+							className="!bg-success"
+							variant="solid"
+							onClick={() =>
+								toast.success("Toast Success", {
+									action: {
+										label: "Action",
+										onClick: () => {},
+									},
+									cancel: {
+										label: "Cancel",
+										onClick: () => {},
+									},
+								})
+							}
+						>
+							Success
+						</Button>
+						<Button
+							color="default"
+							className="!bg-warning"
+							variant="solid"
+							onClick={() =>
+								toast.warning("Toast Warning", {
+									action: {
+										label: "Action",
+										onClick: () => {},
+									},
+									cancel: {
+										label: "Cancel",
+										onClick: () => {},
+									},
+								})
+							}
+						>
+							Warning
+						</Button>
+						<Button
+							color="default"
+							className="!bg-error"
+							variant="solid"
+							onClick={() =>
+								toast.error("Toast Error", {
+									action: {
+										label: "Action",
+										onClick: () => {},
+									},
+									cancel: {
+										label: "Cancel",
+										onClick: () => {},
+									},
+								})
+							}
+						>
+							Error
+						</Button>
+					</Flex>
 				</Card>
 			</Flex>
 
 			<Flex gap={16} wrap>
-				<Card title="Success" bordered={false} className="flex-none lg:flex-1">
-					Success
+				<Card title="Position" bordered={false} className="flex-none lg:flex-1">
+					<Flex gap={16} wrap>
+						<Button
+							onClick={() => toast.info("Toast Info", { position: "top-left" })}
+						>
+							Top Left
+						</Button>
+						<Button
+							onClick={() =>
+								toast.info("Toast Info", { position: "top-center" })
+							}
+						>
+							Top Center
+						</Button>
+						<Button
+							onClick={() =>
+								toast.info("Toast Info", { position: "top-right" })
+							}
+						>
+							Top Right
+						</Button>
+						<Button
+							onClick={() =>
+								toast.info("Toast Info", { position: "bottom-right" })
+							}
+						>
+							Bottom Right
+						</Button>
+						<Button
+							onClick={() =>
+								toast.info("Toast Info", { position: "bottom-center" })
+							}
+						>
+							Bottom Center
+						</Button>
+						<Button
+							onClick={() =>
+								toast.info("Toast Info", { position: "bottom-left" })
+							}
+						>
+							Bottom Left
+						</Button>
+					</Flex>
 				</Card>
-				<Card title="Warning" bordered={false} className="flex-none lg:flex-1">
-					Warning
+				<Card
+					title="With Promise"
+					bordered={false}
+					className="flex-none lg:flex-1"
+				>
+					<Flex gap={16} justify="center" align="center" wrap>
+						<Button onClick={handleSubmit}>On Submit</Button>
+					</Flex>
 				</Card>
 			</Flex>
 		</Flex>
