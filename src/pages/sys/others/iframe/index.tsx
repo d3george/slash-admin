4506513a7dfbce34b54a19025d72dead3caeef52
d3george <kevin@slashspaces.com>
2024-11-20 12:59:32 +0800

    fix: white screen during loading in iframe

--- src/pages/sys/others/iframe/index.tsx
+++ src/pages/sys/others/iframe/index.tsx
@@ -1,10 +1,26 @@
+import { CircleLoading } from "@/components/loading";
+import { useState } from "react";
+
 type Props = {
 	src: string;
 };
 export default function Iframe({ src = "" }: Props) {
+	const [isLoading, setIsLoading] = useState(true);
+
+	const handleLoad = () => {
+		setIsLoading(false);
+	};
+
 	return (
-		<div className="h-full w-full">
-			<iframe src={src} title="iframe-page" className="h-full w-full" />
+		<div className="h-full w-full relative">
+			{isLoading && <CircleLoading />}
+
+			<iframe
+				src={src}
+				title="iframe-page"
+				className="h-full w-full"
+				onLoad={handleLoad}
+			/>
 		</div>
 	);
 }
