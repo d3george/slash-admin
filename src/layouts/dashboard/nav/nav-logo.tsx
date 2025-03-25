edca74ecbd024c2b2d00a5e8968eef1559e7156d
seal <yejunhai2001@gmail.com>
2025-03-25 16:25:20 +0800

    fix: center collapsed icon

--- src/layouts/dashboard/nav/nav-logo.tsx
+++ src/layouts/dashboard/nav/nav-logo.tsx
@@ -1,6 +1,5 @@
 import Logo from "@/components/logo";
 import { useSettings } from "@/store/settingStore";
-import { cn } from "@/utils";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { ThemeLayout } from "#/enum";
 import { HEADER_HEIGHT } from "../config";
@@ -21,9 +20,7 @@ export default function NavLogo({ collapsed, onToggle }: Props) {
 			<div
 				onClick={onToggle}
 				onKeyDown={onToggle}
-				className={cn(
-					"absolute right-0 top-7 z-50 hidden h-6 w-6 translate-x-1/2 cursor-pointer select-none items-center justify-center rounded-full text-center md:flex border border-dashed border-border text-sm bg-bg-paper",
-				)}
+				className="absolute right-0 transition-all top-1/2 z-50 hidden size-6 translate-x-1/2 -translate-y-1/2 cursor-pointer select-none items-center justify-center rounded-full text-center md:flex border border-dashed border-border text-sm bg-bg-paper"
 			>
 				{collapsed ? (
 					<RightOutlined className="text-xs text-text-disabled" />
