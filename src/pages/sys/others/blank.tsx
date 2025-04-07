4fb5501134032af938fcbbb55566f063d42155f7
d3george <kevin@slashspaces.com>
2025-04-07 23:43:43 +0800

    feat: nav vercital develop

--- src/pages/sys/others/blank.tsx
+++ src/pages/sys/others/blank.tsx
@@ -1,13 +1,14 @@
 import Card from "@/components/card";
-import { NAV_SECTION_ITEMS, NavVertical } from "@/components/nav";
-import { ScrollArea } from "@/ui/scroll-area";
+// import { NAV_SECTION_ITEMS, NavVertical } from "@/components/nav";
+// import { ScrollArea } from "@/ui/scroll-area";
 
 export default function BlankPage() {
 	return (
-		<Card className="w-80 h-[400px] flex flex-col justify-start">
-			<ScrollArea className="w-full h-full pr-2">
-				<NavVertical data={NAV_SECTION_ITEMS} />
-			</ScrollArea>
-		</Card>
+		// <Card className="w-80 h-[400px] flex flex-col justify-start">
+		// 	<ScrollArea className="w-full h-full pr-2">
+		// 		<NavVertical data={NAV_SECTION_ITEMS} />
+		// 	</ScrollArea>
+		// </Card>
+		<Card />
 	);
 }
