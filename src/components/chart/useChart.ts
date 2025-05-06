b84e4ee91c4408fdd0d9201af74a1cc903a8c0ab
d3george <kevin@slashspaces.com>
2025-05-06 09:57:51 +0800

    fix: chart tooltip theme mode

--- src/components/chart/useChart.ts
+++ src/components/chart/useChart.ts
@@ -8,7 +8,7 @@ import { breakpointsTokens } from "@/theme/tokens/breakpoints";
 import { paletteColors, presetsColors } from "@/theme/tokens/color";
 
 export function useChart(options: ApexOptions) {
-	const { themeColorPresets } = useSettings();
+	const { themeColorPresets, themeMode } = useSettings();
 
 	const LABEL_TOTAL = {
 		show: true,
@@ -111,7 +111,7 @@ export function useChart(options: ApexOptions) {
 
 		// Tooltip
 		tooltip: {
-			theme: undefined,
+			theme: themeMode,
 			x: {
 				show: true,
 			},
